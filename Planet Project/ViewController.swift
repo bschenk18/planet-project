5ba1c4111ff2850c03b46aec4d04e20386ff807f
Benjamin Prentiss <bschenk18@yahoo.com>
2022-12-13 11:24:09 -0700

    Initial Commit

---
+++ Planet Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planet Project
+//
+//  Created by Benjamin Prentiss on 12/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
